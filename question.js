cc7a09e5ec385abf1be097377bd171e4c558d5b3
radiation22 <radiationcorporation2@gmail.com>
2022-01-07 21:35:12 +0600

    first commit

---
+++ question.js
@@ -0,0 +1,59 @@
+const questions = [
+    {
+        numb: 1,
+        question: "What Does HTML stand for?",
+        answer: "Hyper Text Markup Language",
+        option: [
+            "Hyper Text Preprocessor",
+            "Hyper Text Markup Language",
+            "Hyper Text Multiple Language",
+            "Hyper Text Multi Language"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What Does CSS stand for?",
+        answer: "Cascading Style Sheet",
+        option: [
+            "Common Style Sheet",
+            "Colorful Style Sheet",
+            "Computer Style Sheet",
+            "Cascading Style Sheet"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What Does PHP stand for?",
+        answer: "Hypertext Preprocessor",
+        option: [
+            "Hypertext Preprocessor",
+            "Hypertext Programming",
+            "Hypertext Pre-programing",
+            "Hometext Preprocessor"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What Does SQL stand for?",
+        answer: "Structured Query Language",
+        option: [
+            "Stylish Question Language",
+            "Structured Query Language",
+            "Statement Query Language",
+            "Stylesheet Query Language"
+        ]
+    },
+    {
+        numb: 5,
+        question: "What Does XML stand for?",
+        answer: "Extensible Markup Language",
+        option: [
+            "Extensible Markup Language",
+            "Extensible Multiple Language",
+            "Extra Multi-programme Language",
+            "Examine Multiple Language"
+        ]
+    },
+    
+
+]
